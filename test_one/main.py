95646f6f01591d350f1ef165616334a66771cd41
ShaneJBeuning <131703529+ShaneJBeuning@users.noreply.github.com>
2025-05-13 10:42:51 -0600

    initial (#1)
    * initial
    
    * remove solutions
    
    * readme change

---
+++ test_one/main.py
@@ -0,0 +1,29 @@
+# We have forecasts that can come in for different locations at different times
+# We want to be able to get the forecast for a specific location and time
+
+def main():
+    """Fill in the code below the"""
+
+    forecasts_path = "testing_forecast_csv.csv"
+    # reference_datetime_utc is the time that the forecast was created
+    # validity_datetime_utc is the forecasted hour
+    # forecast_load is the forecasted load and can rounded to 2 decimal places
+    # unit is the unit of the forecast_load w, kw, or mw
+
+    # TODO
+    # One: Get forecast for HCE at 2025-05-02 02:00:00.000000 UTC
+
+    # Two: Get the most recent forecast from all locations
+
+    # Three: Get the most recent forecast from a specific location
+
+    # Four: Convert all load values to mw
+
+
+    print("You got this!")
+
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ test_zero/main.py
@@ -0,0 +1,17 @@
+
+# Assume the first day of any year is a Monday, find if the day a Sunday
+
+def is_sunday(day):
+    #TODO
+
+
+def main():
+    # Example date
+    day = 303
+    if is_sunday(day):
+        print(f"{day} is a Sunday.")
+    else:
+        print(f"{day} is not a Sunday.")
+
+if __name__ == "__main__":
+    main()
